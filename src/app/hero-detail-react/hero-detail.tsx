14fb171f7b3cc5ca89cfb0df34487a0963d7bc1f
Paulo Wayner <paulowsantos@gmail.com>
2022-10-06 10:28:41 -0600

    added hero detail react component

---
+++ src/app/hero-detail-react/hero-detail.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { FunctionComponent } from 'react';
+
+const HeroDetailReact: FunctionComponent = () => {
+  return (
+    <span>Hero detail page</span>
+  );
+}
+export default HeroDetailReact;
